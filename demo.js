bc366eb24bd59e1b52a5e2f49495e8b7eaed49e5
Yuvashree M <128991477+YuvashreeMagesh@users.noreply.github.com>
2024-04-29 13:58:11 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,21 +1,21 @@
-const readline = require('readline');
+// const readline = require('readline');
 
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-rl.question('Enter a number: ', (number) => {
-  // Convert input string to a number
-  number = parseInt(number);
+// rl.question('Enter a number: ', (number) => {
+//   // Convert input string to a number
+//   number = parseInt(number);
 
-  // Check if the number is even or odd
-  if (number % 2 === 0) {
-    console.log(`${number} is even.`);
-  } else {
-    console.log(`${number} is odd.`);
-  }
+//   // Check if the number is even or odd
+//   if (number % 2 === 0) {
+//     console.log(`${number} is even.`);
+//   } else {
+//     console.log(`${number} is odd.`);
+//   }
 
-  rl.close();
-});
+//   rl.close();
+// });
